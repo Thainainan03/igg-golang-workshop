32ea09866ce311cfc8b5c9c3a06a73a52a7cbdeb
Thanainan Khamthaen <toypoyomn03@outlook.com>
2021-05-18 01:02:53 +0700

    deck

--- basic/main.go
+++ basic/main.go
@@ -3,14 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	cards := []string{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-	for index, card := range cards {
-		fmt.Println(index, card)
-	}
-	fmt.Println(cards)
-}
-
-func newCard() string {
-	return "Ace of spades"
+	cards := newDeckFromFile("deck.txt")
+	fmt.Println(cards.shuffle())
+	// cards.print()
+	// fmt.Println(time.Now().UnixNano())
 }
