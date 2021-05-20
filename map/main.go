119c483f531c600f83fd55c35477b08a9c619b6b
Thanainan Khamthaen <toypoyomn03@outlook.com>
2021-05-20 18:03:29 +0700

    map

---
+++ map/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// colors := map[string]string{
+	// 	"red":   "#FF0000",
+	// 	"green": "#00FF00",
+	// 	"blue":  "#0000FF",
+	// }
+	ages := map[string]int{
+		"set": 39,
+	}
+	fmt.Println(ages)
+}
